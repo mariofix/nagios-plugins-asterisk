e45bede02aa408b4a2ea4f4e34dd6885bbcdc667
Mario Hernandez <yo@mariofix.com>
2014-02-10 18:14:51 -0300

    licence info

--- check_asterisk_alive.sh
+++ check_asterisk_alive.sh
@@ -1,4 +1,30 @@
 #!/bin/sh
+#============================== SUMMARY =====================================
+#
+#Program : check_asterisk_alive
+#Version : 0.1
+#Date : Feb 10, 2014 
+#Author : Mario Hernandez - @mariofix 
+#Licence : GPL - summary below, full text at http://www.fsf.org/licenses/gpl.txt 
+#Requirements: asterisk - bash
+#
+#=========================== PROGRAM LICENSE =================================
+#
+#This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+#This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 675 Mass
+# Ave, Cambridge, MA 02139, USA.
+#
+
+
 ASTPID=`cat /var/run/asterisk/asterisk.pid`
 ASTRUN=`ps --no-heading p $ASTPID | wc -l`
 
